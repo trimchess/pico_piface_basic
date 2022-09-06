364f38566507d8a2f3495992539056a0fd5fc5a2
trimchess <pycoder.maker@gmail.com>
2022-09-06 15:30:54 +0200

    freertos added

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,64 @@
 #include "mcp23s17.h"
 #include <iostream>
 #include <iomanip>
+#include <FreeRTOS.h>
+#include <task.h>
 
 #define LED_PIN 10
 
+MCP23S17 mcp;
+
 using namespace std;
+
+void printValue(int, const char *);
+
+void led10_task()
+{
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    const TickType_t xDelay = 500 / portTICK_PERIOD_MS; // 500ms
+    while (true)
+    {
+        gpio_put(LED_PIN, HIGH);
+        vTaskDelay(2 * xDelay);
+        gpio_put(LED_PIN, LOW);
+        vTaskDelay(2 * xDelay);
+    }
+}
+
+void mcp_out_task()
+{
+    mcp.writeRegister(IODIRB, 0xFF);                    // port B (pins 1-7) all bits OUTPUT
+    mcp.writeRegister(IPOLB, 0xFF);                     // port B bits 7...0 inverted
+    mcp.writeRegister(GPPUB, 0xFF);                     // port B pull up all inputs.
+    const TickType_t xDelay = 500 / portTICK_PERIOD_MS; // 500ms
+    while(1)
+    {
+        vTaskDelay(xDelay * 2);
+        uint8_t readResult = mcp.readRegister(GPIOB); // read input direct via GPIO. No latch for inputs!
+        printValue((int)readResult, "Read Data Register GPIOB: 0x");
+    }
+
+}
+
+void mcp_in_task()
+{
+    mcp.writeRegister(IODIRA, 0x00);                    // port A (pins 17-24) all bits INPUT
+    const TickType_t xDelay = 500 / portTICK_PERIOD_MS; // 500ms
+    while (1)
+    {
+        mcp.writeRegister(OLATA, 0xAA); // set output via latch register
+        vTaskDelay(xDelay * 2);
+        mcp.writeRegister(OLATA, 0x55); // set output via latch register
+        vTaskDelay(xDelay * 2);
+    }
+}
 
 /**
  * @brief Printaout formated value
  *
  * @param value
  */
-
 void printValue(int value, const char *theString)
 {
     cout << theString << hex
@@ -43,43 +90,10 @@ void printValue(int value, const char *theString)
  */
 int main(void)
 {
-    const uint8_t DEVICE_ADDR = 0x40;
-
-    /**
-     * @brief Construct a new stdio init all object
-     *
-     */
     stdio_init_all();
-    // set Pin 10 as flow control LED
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    // instantiate an MCP23S17 object
-    MCP23S17 mcp;
-    // start mcp with default values
-    // spi0, pico GPIO2 = SCK, pico GPIO3 = MOSI, GPIO4 = MISO, pico GPIO = CS
     mcp.begin();
-
-    // PiFace
-    // GPA > OUTPUT, MCP23S17 Pin  21...28
-    // GPB > INPUT,  MCP23S17 Pin  1...8
-    mcp.writeRegister(IODIRA, 0x00); // port A (pins 17-24) all bits INPUT
-    mcp.writeRegister(IODIRB, 0xFF); // port B (pins 1-7) all bits OUTPUT
-    mcp.writeRegister(IPOLB, 0xFF);  // port B bits 7...0 inverted
-    mcp.writeRegister(GPPUB, 0xFF);  // port B pull up all inputs.
-
-    while (1)
-    {
-        mcp.writeRegister(OLATA, 0xAA);               // set output via latch register
-        uint8_t readResult = mcp.readRegister(OLATA); // read output via latch register
-        //printValue((int)readResult, "Read Data Register GPIOA: 0x");
-        gpio_put(LED_PIN, 1);
-        sleep_ms(1000);
-        mcp.writeRegister(OLATA, 0x55);       // set output via latch register
-        readResult = mcp.readRegister(OLATA); // read output via latch register
-        //printValue((int)readResult, "Read Data Register GPIOA: 0x");
-        readResult = mcp.readRegister(GPIOB); // read input direct via GPIO. No latch for inputs!
-        printValue((int)readResult, "Read Data Register GPIOB: 0x");
-        gpio_put(LED_PIN, 0);
-        sleep_ms(1000);
-    }
+    xTaskCreate((TaskFunction_t)led10_task, "LED10_TASK", 256, NULL, 1, NULL);
+    xTaskCreate((TaskFunction_t)mcp_in_task, "MCPIN_TASK", 256, NULL, 1, NULL);
+    xTaskCreate((TaskFunction_t)mcp_out_task, "MCPOUT_TASK", 256, NULL, 1, NULL);
+    vTaskStartScheduler();
 }
